2e2f7a6d8472bc6b1a08489c7b7f1fd585e20954
NAGIBATOR2003 <nick.chadromtsev@gmail.com>
2018-05-11 15:37:35 +0300

    Add files via upload

---
+++ iluhaApp.py
@@ -0,0 +1,115 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+
+
+
+
+#def pause():
+ #   paused = True
+  #  message_to_screen("Paused", black, -100, size="large")
+   # message_to_screen("Press c to continue , q to quit", black, 25)
+    #pygame.display.update()
+    #while paused:
+     #   for event in pygame.event.get():
+      #      if event.type == pygame.QUIT:
+       #         pygame.quit()
+        #        quit()
+#
+ #           if event.type == pygame.KEYDOWN:
+  #              if event.key == pygame.K_c:
+   #                 paused = False
+#
+ #               elif event.key == pygame.K_q:
+  ##                 quit()
+    #    # gameDisplay.fill(white)
+     #   clock.tick(5)
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced
+            ball.velocity = vel.x, vel.y + offset
+            #bounced = Vector(-1*vx, -1*vy)
+            #vel = bounced
+            #offset = (ball.center_y - touch.y)/2
+            #ball.velocity = vel.x, vel.y + offset
+
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+    btn = ObjectProperty(None)
+
+    def serve_ball(self, vel=(0, 0)):
+        self.ball.center = self.center
+        self.ball.velocity = vel
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce of paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+
+
+        # bounce ball off bottom or top
+        if (self.ball.y < self.y) or (self.ball.top > self.top):
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right and scoaring of points
+        if self.ball.x < 0:
+            if (self.ball.y > self.height/3) and (self.ball.y < 2*self.height/3):
+                self.player2.score += 1
+                self.serve_ball(vel=(4, 0))
+            else:
+                self.ball.velocity_x *= -1
+        if self.ball.x > self.width:
+            if (self.ball.y > self.height / 3) and (self.ball.y < 2 * self.height / 3):
+                self.player1.score += 1
+                #self.player1.score = acc_x1
+                self.serve_ball(vel=(-4, 0))
+            else:
+                self.ball.velocity_x *= -1
+
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 2:
+            self.player1.center_y = touch.y
+            self.player1.center_x = touch.x
+        if touch.x > self.width - self.width / 2:
+            self.player2.center_y = touch.y
+            self.player2.center_x = touch.x
+
+
+class PongApp(App):
+    def build(self):
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0 / 600.0)
+        btn = Button(text = "Pause")
+
+        return game
+
+
+if __name__ == '__main__':
+    PongApp().run()
